27f4f7aec2efe2d64ff00020f40848bdcd2aa9b3
Piyush Thange <pthange19@gmail.com>
2021-10-04 13:04:11 +0530

    Take Username and welcome+User.
    Jeffy, who is going to complete the higher education in this year needs to create a simple application which accept the name of a person and welcome them with a message along with their name. She wants to read the data using the class "Scanner". Implement this scenario using Java.
    
    Sample Input 1:
    
    Enter the name:
    
    Johson
    
    Sample Output 1:
    
    Welcome Johnson.
    
    Sample Input 2:
    
    Enter the name:
    
    Stain Polson
    
    Sample Output 2:
    
    Welcome Stain Polson.
    
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ Username.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Username{
+    public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String name = sc.nextLine();
+        //sc.nextLine();
+        System.out.println("Welcome " + name);
+        
+    }
+}
