1017ce8036b15d7054c53bdce9bc5f5948edc21d
Piyush Thange <pthange19@gmail.com>
2021-10-12 18:48:54 +0530

    Travel Details
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ class_obj_pkg/3_Person.java
@@ -0,0 +1,27 @@
+public class 3_Person{
+    private String name;
+    private char gender;
+    private int age;
+    
+    //getters
+    public String getName(){
+        return name;
+    }
+    public char getGender(){
+        return gender;
+    }
+    public int getAge(){
+        return age;
+    }
+    
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setGender(char gender){
+        this.gender = gender;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
